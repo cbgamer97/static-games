b86308d7e7796d689aaf559feb1ae20024feccaf
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-02-21 12:47:45 -0600

    starting to lay down stats for static game

---
+++ class-stats.js
@@ -0,0 +1,80 @@
+"use strict";
+
+// CLASSES
+
+
+const classes = [
+    {
+        name: "Paladin",
+        hitPoints: 230,
+        maxDamage: 15,
+        maxMana: 250,
+        strength: 10,
+        stamina: 12,
+        intellect: 5,
+        haste: 9,
+        agility: 7
+    },
+    {
+        name: "Wizard",
+        hitPoints: 150,
+        maxDamage: 20,
+        maxMana: 300,
+        strength: 5,
+        stamina: 10,
+        intellect: 15,
+        haste: 9,
+        agility: 4
+    },
+    {
+        name: "Ranger",
+        hitPoints: 200,
+        maxDamage: 18,
+        maxEnergy: 100,
+        strength: 9,
+        stamina: 12,
+        intellect: 9,
+        haste: 12,
+        agility: 12
+    },
+    {
+        name: "Rogue",
+        hitPoints: 120,
+        maxDamage: 22,
+        maxEnergy: 100,
+        strength: 8,
+        stamina: 9,
+        intellect: 6,
+        haste: 15,
+        agility: 18
+    },
+    {
+        name: "Cleric",
+        hitPoints: 150,
+        maxDamage: 14,
+        maxMana: 400,
+        strength: 5,
+        stamina: 10,
+        intellect: 12,
+        haste: 11,
+        agility: 4
+    },
+    {
+        name: "Barbarian",
+        hitPoints: 300,
+        maxDamage: 20,
+        maxRage: 100,
+        strength: 15,
+        stamina: 15,
+        intellect: 3,
+        haste: 7,
+        agility: 4
+    }
+]
+
+// WEARABLES
+
+const equips = [
+
+]
+
